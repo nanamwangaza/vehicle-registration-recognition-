c000f1981231dfa65858a45f578f58ebff1fc6d7
nanamwangaza <84356032+nanamwangaza@users.noreply.github.com>
2023-08-30 09:56:37 +0300

    deleted unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:vehicle_registration/pages/camera_page.dart';
-// import 'package:vehicle_registration/pages/geolocator.dart';
-// import 'package:vehicle_registration/pages/home.dart';
-// import 'package:vehicle_registration/pages/home_pro.dart';
-// import 'package:vehicle_registration/pages/launch_camera.dart';
-// import 'package:vehicle_registration/pages/login_page.dart';
-// import 'package:vehicle_registration/pages/signup.dart';
-import 'package:vehicle_registration/pages/camera_page.dart';
-import 'package:vehicle_registration/pages/geolocator.dart';
-import 'package:vehicle_registration/pages/home.dart';
-import 'package:vehicle_registration/pages/home_pro.dart';
-import 'package:vehicle_registration/pages/launch_camera.dart';
-import 'package:vehicle_registration/pages/login_page.dart';
-
-import 'package:vehicle_registration/pages/signup.dart';
-import 'package:vehicle_registration/pages/user_profile.dart';
 import 'package:vehicle_registration/pages/welcome_page.dart';
 
 
